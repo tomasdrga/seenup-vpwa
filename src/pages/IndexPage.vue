8966625aec35606165ed65901837cd156449562d
Tomáš Drga <tomasxdrga@gmail.com>
2024-09-28 21:16:23 +0200

    Testing quasar components, implementation of the base slack layout

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,47 +1,121 @@
 <template>
-  <q-page class="row items-center justify-evenly">
-    <example-component
-      title="Example component"
-      active
-      :todos="todos"
-      :meta="meta"
-    ></example-component>
+  <q-page>   
+    
+    <div class="full-height row no-wrap justify-start items-start content-start" id="main">
+      <div class="gt-sm col-md-2 full-height" id="left-side-bar">
+        <q-list dense bordered padding>
+          <q-item clickable v-ripple>
+            <q-item-section>
+              Item
+            </q-item-section>
+          </q-item>
+
+          <q-item clickable v-ripple>
+            <q-item-section>
+              Item
+            </q-item-section>
+          </q-item>
+
+          <q-item clickable v-ripple>
+            <q-item-section>
+              Item
+            </q-item-section>
+          </q-item>
+        </q-list>
+      </div>
+      <div class="col-12 col-md-10 full-height column justify-between">
+          <div class="row">
+            <div style="width: 100%;">
+              <q-chat-message
+                label="Sunday, 19th"
+              />
+
+              <div class="row" id="message">
+                  <div class="col-1">
+                    <q-avatar rounded>
+                      <img src="https://cdn.quasar.dev/img/avatar.png">
+                    </q-avatar>
+                  </div>
+                  <div class="col-auto">
+                    <div class="row">
+                      <span class="q-mr-md text-weight-bold">Johno</span>
+                      <div>Yesterday 11:45AM</div>
+                    </div>
+                    <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias incidunt qui nemo. Quod earum recusandae soluta aliquid mollitia, assumenda optio. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Velit, qui? A error fuga, culpa distinctio ut alias beatae quia, tenetur perspiciatis accusamus quis ex. Quo facere sit deserunt debitis non.</div>
+                  </div>
+              </div>
+
+              <div class="row" id="message">
+                  <div class="col-1">
+                    <q-avatar rounded>
+                      <img src="https://cdn.quasar.dev/img/avatar.png">
+                    </q-avatar>
+                  </div>
+                  <div class="col-auto">
+                    <div class="row">
+                      <span class="q-mr-md text-weight-bold">Johno</span>
+                      <div>Yesterday 11:45AM</div>
+                    </div>
+                    <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias incidunt qui nemo. Quod earum recusandae soluta aliquid mollitia, assumenda optio. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Velit, qui? A error fuga, culpa distinctio ut alias beatae quia, tenetur perspiciatis accusamus quis ex. Quo facere sit deserunt debitis non.</div>
+                  </div>
+              </div>
+
+              <div class="row" id="message">
+                  <div class="col-1">
+                    <q-avatar rounded>
+                      <img src="https://cdn.quasar.dev/img/avatar.png">
+                    </q-avatar>
+                  </div>
+                  <div class="col-auto">
+                    <div class="row">
+                      <span class="q-mr-md text-weight-bold">Johno</span>
+                      <div>Yesterday 11:45AM</div>
+                    </div>
+                    <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias incidunt qui nemo. Quod earum recusandae soluta aliquid mollitia, assumenda optio. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Velit, qui? A error fuga, culpa distinctio ut alias beatae quia, tenetur perspiciatis accusamus quis ex. Quo facere sit deserunt debitis non.</div>
+                  </div>
+              </div>
+            </div>
+          </div>
+          <q-editor v-model="editor" min-height="5rem" />
+      </div>
+    </div>
+
   </q-page>
 </template>
 
-<script setup lang="ts">
-import { ref } from 'vue';
-import { Todo, Meta } from 'components/models';
-import ExampleComponent from 'components/ExampleComponent.vue';
-
-defineOptions({
-  name: 'IndexPage'
-});
-
-const todos = ref<Todo[]>([
-  {
-    id: 1,
-    content: 'ct1'
-  },
-  {
-    id: 2,
-    content: 'ct2'
-  },
-  {
-    id: 3,
-    content: 'ct3'
-  },
-  {
-    id: 4,
-    content: 'ct4'
-  },
-  {
-    id: 5,
-    content: 'ct5'
+<style>
+  #main {
+    min-height: inherit;
   }
-]);
+  .full-height {
+    min-height: inherit;
+  }
+  #left-side-bar {
+    border-right: 1px solid #00000015;
+  }
+  .q-chat-message .q-chat-message--received {
+    width: 250px;
+  }
+  #message {
+    padding: 1rem 1rem;
+    border-bottom: 1px solid #00000015;
+    border-top: 1px solid #00000015;
+  }
+</style>
 
-const meta = ref<Meta>({
-  totalCount: 1200
-});
+<script>
+import { ref } from 'vue'
+
+export default {
+  setup () {
+    const leftDrawerOpen = ref(false)
+
+    return {
+      leftDrawerOpen,
+      toggleLeftDrawer () {
+        leftDrawerOpen.value = !leftDrawerOpen.value
+      }
+    }
+  }
+}
 </script>
