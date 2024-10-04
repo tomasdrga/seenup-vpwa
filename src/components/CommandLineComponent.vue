7b9ff5e73a0bf30f0e0e96beca420a2420c162a0
Matej Drienovský <matejdrienovsky123@gmail.com>
2024-10-05 00:44:17 +0200

    Added command-line component, responsiveness under progress

---
+++ src/components/CommandLineComponent.vue
@@ -0,0 +1,63 @@
+<template>
+    <div class="col command-line">
+      <div>
+        <q-editor
+          v-model="editor"
+          min-height="1rem"
+          max-height="5rem"
+          flat
+          overflow-hidden
+          toolbar-bg="grey"
+          :toolbar="[
+          ['bold', 'italic', 'strike'],
+          ['link'],
+          ['unordered', 'ordered'],
+          ['quote'],
+        ]"
+        />
+      </div>
+      <div class="down-menu-bar">
+        <q-btn round dense flat icon="send" size="sm" class="q-ma-xs"/>
+      </div>
+    </div>
+</template>
+
+<script>
+</script>
+
+<style scoped>
+  /* Input block */
+  .command-line {
+    background: #FFFFFF;
+    border: 1px solid #00000015;
+    border-radius: 10px;
+    margin-bottom: 0.5rem;
+  }
+
+  /* Input block modifier */
+  @media (max-width: 1024px) {
+    .command-line {
+      max-width: 100vw;
+      margin-inline: 5px;
+    }
+  }
+
+  @media (min-width: 1024px) {
+    .command-line {
+      max-width: 75vw;
+    }
+  }
+
+  @media (min-width: 1440px) {
+    .command-line {
+      max-width: 76vw;
+    }
+  }
+
+  /* Down menu bar with send button */
+  .down-menu-bar {
+    display: flex;
+    justify-content: end;
+    height: 100%;
+  }
+</style>
--- src/components/MessageComponent.vue
+++ src/components/MessageComponent.vue
@@ -1,24 +1,24 @@
 <template>
   <div class="row no-wrap" id="message">
     <div class="q-pr-md">
-      <q-avatar rounded>
+      <q-avatar rounded class="q-mt-xs">
         <img src="https://cdn.quasar.dev/img/avatar.png">
       </q-avatar>
     </div>
     <div>
-      <div class="row">
-        <span class="q-mr-md text-weight-bold">{{userName}}</span>
-        <div>{{time}}</div>
+      <div class="row message-info-row">
+        <span class="q-mr-md text-weight-bold text-body1">{{ userName }}</span>
+        <div class="disabled text-caption">{{ time }}</div>
       </div>
-      <div>{{message}}</div>
+      <div v-html="processedMessage" class="text-body2"></div>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import { defineProps } from 'vue';
+import { defineProps,  computed } from 'vue';
 
-defineProps({
+const props = defineProps({
   userName: {
     type: String,
     required: true
@@ -32,4 +32,25 @@ defineProps({
     required: true
   },
 });
+
+const processedMessage = computed(() => {
+  let text = props.message.replace(/</g, '&lt;').replace(/>/g, '&gt;');
+
+  text = text.replace(/\B@([\w]+)/g, '<mark>@$1</mark>');
+
+  return text;
+});
 </script>
+
+<style>
+  mark {
+    background-color: rgba(38, 0, 101, 0.15);
+    color: #260065;
+    margin-right: 2px;
+  }
+
+  .message-info-row {
+    display: flex;
+    align-items: center;
+  }
+</style>
