dfd87dfe851584810b55d2c648b1eb24a5d8e3e2
Tomáš Drga <tomasxdrga@gmail.com>
2024-10-15 20:46:49 +0200

    Main layout major changes, responsive, slack inspired

--- src/layouts/IndexLayout.vue
+++ src/layouts/IndexLayout.vue
@@ -1,49 +1,145 @@
 <template>
-  <q-layout view="lHh lpr lFf" class="bg-grey">
-    <q-header class="bg-grey text-purple" height-hint="98">
-      <q-toolbar>
-        <q-toolbar-title class="text-weight-bold">
-          <q-avatar>
-            <img src="https://cdn.quasar.dev/logo-v2/svg/logo-mono-white.svg">
+  <q-layout view="lHh lpr lFf" class="bg-grey no-scroll">
+    <q-header class="bg-grey text-primary main-header" height-hint="98">
+     <q-toolbar class="row items-center justify-between">
+      <!-- Title with Avatar on the Left -->
+      <q-toolbar-title class="text-weight-bold row items-center">
+        <div class="col items-center">
+          <q-avatar size="2rem" class="q-mr-xs">
+            <img src="../assets/nowty_face.png">
           </q-avatar>
-          Title
-        </q-toolbar-title>
+          SeenUp
+        </div>
+        
+        <q-avatar 
+        v-if="$q.screen.lt.sm" 
+        rounded 
+        size="lg"
+        class="q-ml-md">
+        <img src="https://cdn.quasar.dev/img/avatar.png">
+      </q-avatar>
+      </q-toolbar-title>
       </q-toolbar>
+      
 
-      <q-tabs align="left">
-        <q-route-tab to="/page1">
-          <span class="text-weight-bold">Page One</span>
+      <!-- <q-tabs align="left">
+        <q-route-tab to="/messages" id="message-tab">
+          <div class="row items-center">
+             <q-icon name="question_answer" class="q-mr-xs"/>
+            <span class="text-weight-bold">Messages</span>
+          </div>
         </q-route-tab>
-        <q-route-tab to="/page2">
-          <span class="text-weight-bold">Page Two</span>
+        <q-route-tab to="/files" id="files-tab">
+          <div class="row items-center">
+            <q-icon name="layers" class="q-mr-xs"/>
+            <span class="text-weight-bold">Files</span>
+          </div>
         </q-route-tab>
-        <q-route-tab to="/page3">
-          <span class="text-weight-bold">Page Three</span>
+        <q-route-tab to="/settings" id="settings-tab">
+          <div class="row items-center">
+            <q-icon name="settings" class="q-mr-xs"/>
+            <span class="text-weight-bold">Settings</span>
+          </div>
         </q-route-tab>
-      </q-tabs>
+      </q-tabs> -->
     </q-header>
 
-    <q-drawer show-if-above v-model="leftDrawerOpen" side="left" width="100" class="bg-grey">
-      <!-- drawer content -->
+    <q-drawer show-if-above breakpoint="600" v-model="leftDrawerOpen" side="left" width="75" class="bg-grey row content-between justify-center">
+      <div class="row justify-center items-center" id="top_actions">
+        <q-tabs vertical class="text-purple">
+          <q-route-tab to="/messages" id="message-tab">
+            <div class="column items-center">
+              <q-icon name="home"/>
+              <span class="text-weight-bold smaller">Home</span>
+            </div>
+          </q-route-tab>
+          <q-route-tab to="/files" id="files-tab">
+            <div class="column items-center">
+              <q-icon name="notifications"/>
+              <span class="text-weight-bold smaller">Activity</span>
+            </div>
+          </q-route-tab>
+          <q-route-tab to="/settings" id="settings-tab">
+            <div class="column items-center">
+              <q-icon name="more_horiz"/>
+              <span class="text-weight-bold smaller">More</span>
+            </div>
+          </q-route-tab>
+        </q-tabs>
+      </div>
+
+      <div class="row justify-center items-center q-mb-md">
+        <q-btn round icon="add" class="q-mb-md" id="add-button"/>
+        <q-avatar rounded size="lg">
+          <img src="https://cdn.quasar.dev/img/avatar.png">
+        </q-avatar>
+      </div>  
     </q-drawer>
 
     <q-page-container>
       <router-view />
     </q-page-container>
 
-    <q-footer reveal elevated class="bg-grey-8 text-white" v-if="!leftDrawerOpen">
+    <q-footer reveal class="bg-grey text-primary" v-if="!leftDrawerOpen" height-hint="20">
       <q-toolbar>
-        <q-toolbar-title>
-          <q-avatar>
-            <img src="https://cdn.quasar.dev/logo-v2/svg/logo-mono-white.svg">
-          </q-avatar>
-          Title
-        </q-toolbar-title>
+        <q-tabs horizontal class="text-purple row justify-center items-center full-width">
+          <q-route-tab to="/messages" id="message-tab">
+            <div class="column items-center">
+              <q-icon name="home"/>
+              <span class="text-weight-bold smaller">Home</span>
+            </div>
+          </q-route-tab>
+          <q-route-tab to="/files" id="files-tab">
+            <div class="column items-center">
+              <q-icon name="notifications"/>
+              <span class="text-weight-bold smaller">Activity</span>
+            </div>
+          </q-route-tab>
+          <q-route-tab to="/settings" id="settings-tab">
+            <div class="column items-center">
+              <q-icon name="more_horiz"/>
+              <span class="text-weight-bold smaller">More</span>
+            </div>
+          </q-route-tab>
+        </q-tabs>
       </q-toolbar>
     </q-footer>
 
   </q-layout>
 </template>
+
+<style lang="scss">
+  .no-scroll {
+    overflow: hidden;
+  }
+  .q-tabs .q-focus-helper {
+    display: none;
+  }
+  #message-tab:hover, #files-tab:hover, #settings-tab:hover {
+    color: $purple-8;
+  }
+
+  #add-button {
+    background-color: transparent;
+    color: $purple;
+    border:1px solid $purple;
+  }
+
+  #top_actions {
+    margin-top: 3rem;
+  }
+
+  .smaller {
+    font-size: 0.7rem;
+  }
+
+  @media (max-width: $breakpoint-sm-min) {
+    .main-header {
+      margin-bottom: 8px;
+    }
+    
+  }
+</style>
 
 <script>
 import { ref } from 'vue'
--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,112 +1,147 @@
 <template>
-  <q-page>
-    <div class="full-height row no-wrap justify-start items-start content-start rounded bg-white" id="main">
-      <div class="gt-sm col-md-2 full-height" id="left-side-bar">
-        <q-list dense padding>
-          <q-item clickable v-ripple>
-            <q-item-section>
-              Item
-            </q-item-section>
-          </q-item>
+  <q-page class="full-height q-pb-md q-pr-md padding-left-sm">
+    <div class="row no-wrap justify-start items-start content-start rounded bg-white" id="main">
+      <q-btn @click="dialog = true" color="primary" flat class="lt-md" icon="menu"/>
 
-          <q-item clickable v-ripple>
-            <q-item-section>
-              Item
-            </q-item-section>
-          </q-item>
-
-          <q-item clickable v-ripple>
-            <q-item-section>
-              Item
-            </q-item-section>
-          </q-item>
-        </q-list>
+      <q-dialog v-model="dialog" persistent>
+        <div id="side-popup">
+          <div class="row items-center justify-center q-py-md q-pb-xl">
+            <p class="text-smaller q-pl-md text-weight-bold text-primary col-10 q-mb-none">Server name</p>
+            <q-btn @click="dialog = false" flat color="primary" icon="close" class="col-2"/>
+          </div>
+          <div class="text-primary text-body">
+            <q-list class="row rounded-borders text-weight-bold">
+              <q-expansion-item default-opened label="Channels" style="width: 100%;">
+                <q-btn flat align="left" style="width: 100%;">
+                  <q-icon name="tag" size="xs" />
+                  <span class="text-caption">General</span>
+                </q-btn>
+                <q-btn flat align="left" style="width: 100%;">
+                  <q-icon name="tag" size="xs"/>
+                  <span class="text-caption">Standup</span>
+                </q-btn>
+                <q-btn flat align="left" style="width: 100%;">
+                  <q-icon name="lock" size="xs"/>
+                  <span class="text-caption">Projects</span>
+                </q-btn>
+              </q-expansion-item>
+            </q-list>
+          </div>
+        </div>
+      </q-dialog>
+      <div class="col-0 col-md-2 full-height gt-sm" id="left-side-bar">
+        <p :class="$q.screen.lt.lg ? 'text-smaller q-pa-md text-weight-bold text-primary' : 'text-h6 q-pa-md text-weight-bold text-primary'">Server name</p>
+        <div class="text-primary text-body">
+          <q-list class="rounded-borders text-weight-bold">
+            <q-expansion-item default-opened label="Channels" style="width: 100%;">
+              <q-btn flat align="left" style="width: 100%;">
+                <q-icon name="tag" size="xs" />
+                <span class="text-caption">General</span>
+              </q-btn>
+              <q-btn flat align="left" style="width: 100%;">
+                <q-icon name="tag" size="xs"/>
+                <span class="text-caption">Standup</span>
+              </q-btn>
+              <q-btn flat align="left" style="width: 100%;">
+                <q-icon name="lock" size="xs"/>
+                <span class="text-caption">Projects</span>
+              </q-btn>
+            </q-expansion-item>
+          </q-list>
+        </div>
       </div>
       <div class="col-12 col-md-10 full-height column justify-between">
         <div class="row">
           <div style="width: 100%;">
-            <q-chat-message
-              label="Sunday, 19th"
-            />
-            <div style="width: 100%; overflow:auto; max-height: 90%;" class="scroll overflow-auto">
-              <message-component time="Yesterday 11: 45AM" message="Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias incidunt qui nemo. Quod earum recusandae soluta aliquid mollitia, assumenda optio. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Velit, qui? A error fuga, culpa distinctio ut alias beatae quia, tenetur perspiciatis accusamus quis ex. Quo facere sit deserunt debitis non." user-name="Johno"/>
-              <message-component time="Yesterday 11:45AM" message="Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias incidunt qui nemo. Quod earum recusandae soluta aliquid mollitia, assumenda optio. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Velit, qui? A error fuga, culpa distinctio ut alias beatae quia, tenetur perspiciatis accusamus quis ex. Quo facere sit deserunt debitis non." user-name="Johno"/>
-              <message-component time="Yesterday 11:45AM" message="Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias incidunt qui nemo. Quod earum recusandae soluta aliquid mollitia, assumenda optio. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Velit, qui? A error fuga, culpa distinctio ut alias beatae quia, tenetur perspiciatis accusamus quis ex. Quo facere sit deserunt debitis non." user-name="Johno"/>
-              <message-component time="Yesterday 11:45AM" message="Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias incidunt qui nemo. Quod earum recusandae soluta aliquid mollitia, assumenda optio. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Velit, qui? A error fuga, culpa distinctio ut alias beatae quia, tenetur perspiciatis accusamus quis ex. Quo facere sit deserunt debitis non." user-name="Johno"/>
-              <message-component time="Yesterday 11:45AM" message="Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias incidunt qui nemo. Quod earum recusandae soluta aliquid mollitia, assumenda optio. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Velit, qui? A error fuga, culpa distinctio ut alias beatae quia, tenetur perspiciatis accusamus quis ex. Quo facere sit deserunt debitis non." user-name="Johno"/>
-              <message-component time="Yesterday 11:45AM" message="Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias incidunt qui nemo. Quod earum recusandae soluta aliquid mollitia, assumenda optio. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Velit, qui? A error fuga, culpa distinctio ut alias beatae quia, tenetur perspiciatis accusamus quis ex. Quo facere sit deserunt debitis non." user-name="Johno"/>
-              <message-component time="Yesterday 11:45AM" message="Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias incidunt qui nemo. Quod earum recusandae soluta aliquid mollitia, assumenda optio. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Velit, qui? A error fuga, culpa distinctio ut alias beatae quia, tenetur perspiciatis accusamus quis ex. Quo facere sit deserunt debitis non." user-name="Johno"/>
-              <message-component time="Yesterday 11:45AM" message="Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias incidunt qui nemo. Quod earum recusandae soluta aliquid mollitia, assumenda optio. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Velit, qui? A error fuga, culpa distinctio ut alias beatae quia, tenetur perspiciatis accusamus quis ex. Quo facere sit deserunt debitis non." user-name="Johno"/>
-              <message-component time="Yesterday 11:45AM" message="Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias incidunt qui nemo. Quod earum recusandae soluta aliquid mollitia, assumenda optio. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Velit, qui? A error fuga, culpa distinctio ut alias beatae quia, tenetur perspiciatis accusamus quis ex. Quo facere sit deserunt debitis non." user-name="Johno"/>
-              <message-component time="Yesterday 11:45AM" message="Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias incidunt qui nemo. Quod earum recusandae soluta aliquid mollitia, assumenda optio. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Velit, qui? A error fuga, culpa distinctio ut alias beatae quia, tenetur perspiciatis accusamus quis ex. Quo facere sit deserunt debitis non." user-name="Johno"/>
-              <message-component time="Yesterday 11:45AM" message="Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias incidunt qui nemo. Quod earum recusandae soluta aliquid mollitia, assumenda optio. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Velit, qui? A error fuga, culpa distinctio ut alias beatae quia, tenetur perspiciatis accusamus quis ex. Quo facere sit deserunt debitis non." user-name="Johno"/>
-              <message-component time="Yesterday 11:45AM" message="Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias incidunt qui nemo. Quod earum recusandae soluta aliquid mollitia, assumenda optio. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Velit, qui? A error fuga, culpa distinctio ut alias beatae quia, tenetur perspiciatis accusamus quis ex. Quo facere sit deserunt debitis non." user-name="Johno"/>
-              <message-component time="Yesterday 11:45AM" message="Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias incidunt qui nemo. Quod earum recusandae soluta aliquid mollitia, assumenda optio. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Velit, qui? A error fuga, culpa distinctio ut alias beatae quia, tenetur perspiciatis accusamus quis ex. Quo facere sit deserunt debitis non." user-name="Johno"/>
-              <message-component time="Yesterday 11:45AM" message="Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias incidunt qui nemo. Quod earum recusandae soluta aliquid mollitia, assumenda optio. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Velit, qui? A error fuga, culpa distinctio ut alias beatae quia, tenetur perspiciatis accusamus quis ex. Quo facere sit deserunt debitis non." user-name="Johno"/>
-              <message-component time="Yesterday 11:45AM" message="Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias incidunt qui nemo. Quod earum recusandae soluta aliquid mollitia, assumenda optio. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Velit, qui? A error fuga, culpa distinctio ut alias beatae quia, tenetur perspiciatis accusamus quis ex. Quo facere sit deserunt debitis non." user-name="Johno"/>
-              <message-component time="Yesterday 11:45AM" message="Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias incidunt qui nemo. Quod earum recusandae soluta aliquid mollitia, assumenda optio. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Velit, qui? A error fuga, culpa distinctio ut alias beatae quia, tenetur perspiciatis accusamus quis ex. Quo facere sit deserunt debitis non." user-name="Johno"/>
-              <message-component time="Yesterday 11:45AM" message="Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias incidunt qui nemo. Quod earum recusandae soluta aliquid mollitia, assumenda optio. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Velit, qui? A error fuga, culpa distinctio ut alias beatae quia, tenetur perspiciatis accusamus quis ex. Quo facere sit deserunt debitis non." user-name="Johno"/>
-              <message-component time="Yesterday 11:45AM" message="Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias incidunt qui nemo. Quod earum recusandae soluta aliquid mollitia, assumenda optio. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Velit, qui? A error fuga, culpa distinctio ut alias beatae quia, tenetur perspiciatis accusamus quis ex. Quo facere sit deserunt debitis non." user-name="Johno"/>
-            </div>
-
-
           </div>
         </div>
-        <div class="fix-bottom" >
-          <q-editor v-model="editor" min-height="5rem" />
+        <div class="fix-bottom">
         </div>
       </div>
     </div>
-
   </q-page>
 </template>
 
-<style>
+<style lang="scss">
 #main {
-  min-height: inherit;
+  border: 2px solid #00000015;
+  min-height: calc(100vh - 67px);
 }
 
-.full-height {
-  min-height: inherit;
-}
 #left-side-bar {
-  border-right: 1px solid #00000015;
+  border-right: 2px solid #00000015;
+  width: full-width;
+  height: full-height;
+  min-height: calc(100vh - 70px);
 }
+
 .q-chat-message .q-chat-message--received {
   width: 250px;
 }
+
 #message {
   padding: 1rem 1rem;
   border-bottom: 1px solid #00000015;
   border-top: 1px solid #00000015;
 }
+
 .rounded {
-  border-top-left-radius: 10px;
+  border-radius: 10px;
 }
+
 .fix-bottom {
   position: fixed;
   bottom: 0;
   width: 100%;
 }
 
-</style>
+.full-height {
+  min-height: 100%;
+}
 
-<script>
-import { ref } from 'vue'
-import MessageComponent from 'components/MessageComponent.vue';
+.text-smaller {
+  font-size: 1rem;
+}
 
-export default {
-  components: { MessageComponent },
-  setup () {
-    const leftDrawerOpen = ref(false)
-    const editor = ref('')
-    return {
-      leftDrawerOpen,
-      editor,
-      toggleLeftDrawer () {
-        leftDrawerOpen.value = !leftDrawerOpen.value
-      }
-    }
+#side-popup {
+  width: 300px;
+  height: 100vh !important;
+  background-color: white;
+  position: fixed;
+  top: 0;
+  left: 0;
+  z-index: 1000;
+}
+
+body.platform-ios .q-dialog__inner--minimized > div, body.platform-android:not(.native-mobile) .q-dialog__inner--minimized > div {
+  max-height: 100vh;
+}
+
+.channel-button {
+  width: full-width;
+}
+
+@media (max-width: $breakpoint-sm-min) {
+  .padding-left-sm {
+    padding-left: 16px; /* You can adjust this value as needed */
+  }
+
+  #main {
+    min-height: calc(100vh - 124px);
   }
 }
+
+</style>
+
+
+<script>
+// import MessageComponent from 'components/MessageComponent.vue';
+
+export default {
+  data() {
+    return {
+      dialog: false,
+    };
+  },
+};
 </script>
